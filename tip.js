76a7d345dbba02a915128b7611cd1b04ef98765f
Michal Nazarewicz <mina86@mina86.com>
2010-07-11 13:10:51 +0200

    [tip] Added list of attributes and show element info mode
    Via a list of attributes user can easily specify which attributes
    should be displayed.  An onClick attribute has also been added as
    it may be pretty important (since it often changes the meaning of
    href).  Other onClick handlers are not displayed though.
    
    The element info mode makes the tooltip display information about
    element that is hovered.  That is tag name, ID and class.

--- tip.js
+++ tip.js
@@ -2,8 +2,8 @@
  * ==UserScript==
  * @name         ToolTip
  * @namespace    http://mina86.com/
- * @version      0.9
- * @date         2010-06-17
+ * @version      1.0
+ * @date         2010-07-11
  * @author       Michal “mina86” Nazarewicz <mina86@mina86.com>
  * @description  Shows a tooltip in lower left corner when link etc is hovered.
  * @ujs:category browser: enhancements
@@ -19,7 +19,41 @@
 
 	var style = false;
 	/* Alternativell you can leave it commented and copy this to a user CSS. */
-	style = '#tool-tip-text, #tool-tip-text tr, #tool-tip-text td, #tool-tip-text th { margin: 0 !important; padding: 0 !important; border: 0 none #000 !important; color: #000 !important; font-weight: normal !important; border-collapse: collapse !important; text-decoration: none !important; background-color: #F90 !important; } #tool-tip-text { display: none; border: 1px solid #664A00 !important; width: auto !important; z-index: 1000 !important; position: fixed !important; left: 0 !important; bottom: 0 !important; } #tool-tip-text td { padding: 1px !important; font-size: 10px !important; } #tool-tip-text th { padding: 1px !important; font-size: 8px !important; background-color: #9F0 !important; }';
+	style = 'table#tool-tip-text, table#tool-tip-text thead, table#tool-tip-text tbody, table#tool-tip-text tr, table#tool-tip-text tr td, table#tool-tip-text tr th { margin: 0 !important; padding: 0 !important; border: 0 none #000 !important; color: #000 !important; font-weight: normal !important; line-height: 1em !important; border-collapse: collapse !important; text-decoration: none !important; background-color: #F90 !important; width: auto !important; vertical-align: top !important; text-align: left; } table#tool-tip-text { display: none; border: 1px solid #664A00 !important; z-index: 1000 !important; position: fixed !important; left: 0 !important; bottom: 0 !important; } table#tool-tip-text tr td { padding: 1px !important; font-size: 12px !important; } table#tool-tip-text tr th { padding: 1px !important; font-size: 10px !important; background-color: #9F0 !important; } table#tool-tip-text thead tr th { background-color: #FF0 !important; }';
+
+	/*
+	 * A list of attributes to show in the tool tip.  Each element of
+	 * the array is a 4-element array which elements have the
+	 * following meaning:
+	 *
+	 * 1. the attribetu name,
+	 * 2. the text the sow in TH,
+	 * 3. whether attribute is protected,
+	 * 4. class name to add to TR element or false.
+
+	 * Protected attributes are such that are shown even if their
+	 * value is shared with some other attribute.  For instance, in
+	 * default settings if alt and title had the same value only the
+	 * first one will be shown.  However, if href had the same value
+	 * as some other earlier attribute it will be shown regardless.
+	 * This may happen especially if an image is a link and image's
+	 * alt is the same as link's href.  In such cases, the alt won't
+	 * be shown though.
+	 */
+
+	var attrs = [
+		[ 'href',    'H', true , false ],
+		[ 'onclick', 'C', true , false ],
+		[ 'alt',     'A', false, false ],
+		[ 'title',   'T', false, false ],
+		[ 'src',     'S', false, false ]
+	];
+
+	/*
+	 * Set to true if you want element's information to be displayed
+	 * (tag name, ID, class).
+	 */
+	var showElementInfo = true;
 
 
 	/* No need to touch avything below. */
@@ -27,34 +61,31 @@
 	var handler = function(e) {
 		/* e = e.event; */
 
-		var messages = [], count = 0, target = null;
-		var dbg = '';
-		for (e = e.target; e && e.getAttribute; e = e.parentNode) {
-			dbg += e + "\n";
+		var messages = [], count = 0, target = null, map = [ ];;
 
-			var title = e.getAttribute('title');
-			var alt   = e.getAttribute('alt');
-			var src   = e.getAttribute('src');
-			var href  = e.getAttribute('href');
-			var text  = e.innerText;
-
-			if (title == alt ) alt   = null;
-			if (title == src ) title = null;
-			if (alt   == src ) alt   = null;
-			if (title == href) title = null;
-			if (alt   == href) alt   = null;
-			if (title == text) title = null;
-			if (alt   == text) alt   = null;
-
-			if (href ) messages[count++] = [ 'H' , href  ];
-			if (alt  ) messages[count++] = [ 'A'  , alt   ];
-			if (src  ) messages[count++] = [ 'S'  , src   ];
-			if (title) messages[count++] = [ 'T', title ];
-
-			if (count && !target) {
-				target = e;
-			}
+		if (showElementInfo) {
+			target = e.target;
 		}
+
+		for (e = e.target; e && e.getAttribute; e = e.parentNode) {
+			for (var i = 0, l = attrs.length; i < l; ++i) {
+				var attr = e.getAttribute(attrs[i][0]);
+
+				if (!attr || (!attrs[i][2] && map['attr_' + attr])) {
+					continue;
+				}
+
+				if (map['attr_' + attr] && map['attr_' + attr] > 0) {
+					messages[map['attr_' + attr] - 1] = false;
+				}
+
+				map['attr_' + attr] = attrs[i][2] ? -1 : (count + 1);
+				messages[count++] = [ attrs[i][1], attr, attrs[i][3] ];
+			}
+
+			if (count && !target) target = e;
+		}
+		map = null;
 
 		if (target == old_target) {
 			return;
@@ -66,12 +97,51 @@
 			content.removeChild(content.firstChild);
 		}
 
-		if (count == 0) {
+		if (count == 0 && !showElementInfo) {
 			return;
 		}
 
+
+		if (showElementInfo) {
+			var thead = doc.createElement('thead');
+			var row = doc.createElement('tr');
+			var th  = doc.createElement('th');
+			th.setAttribute('colspan', '2');
+
+			var span = doc.createElement('span');
+			span.setAttribute('class', 'tool-tip-tagname');
+			span.innerText = target.tagName.toLowerCase();
+			th.appendChild(span);
+
+			var tmp = target.getAttribute('id');
+			if (tmp) {
+				span = doc.createElement('span');
+				span.setAttribute('class', 'tool-tip-id');
+				span.innerText = '#' + tmp;
+				th.appendChild(span);
+			}
+
+			tmp = target.getAttribute('class');
+			if (tmp) {
+				span = doc.createElement('span');
+				span.setAttribute('class', 'tool-tip-class');
+				span.innerText = '.' + tmp.replace(' ', '.');
+				th.appendChild(span);
+			}
+
+			row.appendChild(th);
+			thead.appendChild(row);
+			content.appendChild(thead);
+		}
+
+
+		var tbody = count ? doc.createElement('tbody') : null;
 		for (var i = 0; i < count; ++i) {
 			var row = doc.createElement('tr');
+			if (messages[i][2]) {
+				row.setAttribute('class', messages[i][2]);
+			}
+
 			var th  = doc.createElement('th');
 			var td  = doc.createElement('td');
 
@@ -80,9 +150,12 @@
 			row.appendChild(th);
 			row.appendChild(td);
 
-			content.appendChild(row);
+			tbody.appendChild(row);
 		}
 
+		if (tbody) {
+			content.appendChild(tbody);
+		}
 		content.style.display = 'table';
 	};
 
